8fc4ec1a3adceae8186af3079011b5d8a8a3e125
losint <55887105+losint-code@users.noreply.github.com>
2021-08-24 10:51:55 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+OwnerID: ["812782699348492308"],    
+GuildID: "855058416102670336",
+VoiceID: "855058417402773518",
+Token: "ODU2NTIzNDg5NDM4ODU5Mjk0.YNCRqQ.IFYyZDkTMqJK10k32rG2C4su6Wo",
+Prefix: ["!", "."],
+URL: "mongodb+srv://los05:OPV0aoqvxYRUkhsn@losintdb.u3vty.mongodb.net/los05?retryWrites=true&w=majority",
+
+Tag: "ᛟ",
+SecondaryTag: "•",
+
+ServerMessage: "Losint ❤️ Striga",
+ServerCase: "idle",
+Footer: "Losint ❤️ Striga"
+}
